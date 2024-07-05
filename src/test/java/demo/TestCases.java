f13d7e60cf1a409fa977c70c0fe629e904e1b96f
Dhruval Bhatt <drashtijoshi0@gmail.com>
2024-07-06 01:50:33 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,6 @@
 package demo;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,7 +10,18 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
+import org.testng.Assert;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
@@ -26,29 +37,105 @@ public class TestCases {
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
      */
-    @BeforeTest
-    public void startBrowser()
-    {
-        System.setProperty("java.util.logging.config.file", "logging.properties");
 
-        // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
+         @BeforeTest
+        public void startBrowser()
+        {
+            System.setProperty("java.util.logging.config.file", "logging.properties");
+    
+            // NOT NEEDED FOR SELENIUM MANAGER
+            // WebDriverManager.chromedriver().timeout(30).setup();
+    
+            ChromeOptions options = new ChromeOptions();
+            LoggingPreferences logs = new LoggingPreferences();
+    
+            logs.enable(LogType.BROWSER, Level.ALL);
+            logs.enable(LogType.DRIVER, Level.ALL);
+            options.setCapability("goog:loggingPrefs", logs);
+            options.addArguments("--remote-allow-origins=*");
+    
+            System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+    
+            driver = new ChromeDriver(options);
+    
+            driver.manage().window().maximize();
+        }
+    
+    
 
-        ChromeOptions options = new ChromeOptions();
-        LoggingPreferences logs = new LoggingPreferences();
+  @Test
+    public void testCase01() throws InterruptedException, IOException {
+        Wrappers.goToUrlAndWait(driver, "https://www.scrapethissite.com/pages/");
 
-        logs.enable(LogType.BROWSER, Level.ALL);
-        logs.enable(LogType.DRIVER, Level.ALL);
-        options.setCapability("goog:loggingPrefs", logs);
-        options.addArguments("--remote-allow-origins=*");
+        List<Map<String, Object>> teamDataList = new ArrayList<>();
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        // Example data collection (replace with your actual logic)
+        for (int i = 0; i < 5; i++) {
+            Map<String, Object> teamData = new HashMap<>();
+            teamData.put("Epoch Time of Scrape", Instant.now().toEpochMilli());
+            teamData.put("Team Name", "Team " + i);
+            teamData.put("Year", 2024);
+            teamData.put("Win %", 0.35 + i * 0.05);
+            teamDataList.add(teamData);
+            System.out.println(teamData);
 
-        driver = new ChromeDriver(options);
+        }
 
-        driver.manage().window().maximize();
+        // Convert to JSON and write to file
+        writeToJsonFile("hockey-team-data.json", teamDataList);
     }
 
+    @Test
+    public void testCase02() throws InterruptedException, IOException {
+        Wrappers.goToUrlAndWait(driver, "https://www.scrapethissite.com/pages/");
+
+        List<Map<String, Object>> oscarDataList = new ArrayList<>();
+
+        // Example data collection (replace with your actual logic)
+        for (int i = 0; i < 5; i++) {
+            Map<String, Object> movieData = new HashMap<>();
+            movieData.put("Epoch Time of Scrape", Instant.now().toEpochMilli());
+            movieData.put("Year", 2024);
+            movieData.put("Title", "Movie " + i);
+            movieData.put("Nomination", 5 + i);
+            movieData.put("Awards", 2 + i);
+            movieData.put("isWinner", i == 0); // Example: First movie is the winner
+            oscarDataList.add(movieData);
+            System.out.println(movieData);
+            System.out.println(oscarDataList);
+
+        }
+
+        // Convert to JSON and write to file
+        writeToJsonFile("oscar-winner-data.json", oscarDataList);
+    }
+
+    // Method to write list of maps to JSON file
+    private void writeToJsonFile(String fileName, List<Map<String, Object>> dataList) throws IOException {
+        FileWriter fileWriter = new FileWriter(fileName);
+        fileWriter.write("[\n");
+        for (int i = 0; i < dataList.size(); i++) {
+            Map<String, Object> data = dataList.get(i);
+            fileWriter.write("\t{\n");
+            int j = 0;
+            for (String key : data.keySet()) {
+                fileWriter.write("\t\t\"" + key + "\": \"" + data.get(key) + "\"");
+                if (j < data.size() - 1) {
+                    fileWriter.write(",");
+                }
+                fileWriter.write("\n");
+                j++;
+            }
+            fileWriter.write("\t}");
+            if (i < dataList.size() - 1) {
+                fileWriter.write(",");
+            }
+            fileWriter.write("\n");
+        }
+        fileWriter.write("]");
+        fileWriter.close();
+    
+}
     @AfterTest
     public void endTest()
     {
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,17 +3,56 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+     public static void goToUrlAndWait(WebDriver driver, String url) {
+        driver.get(url);
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(
+            webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete")
+        );
+    }
+
+    public static void findElementAndClick(WebDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        element.click();
+    }
+
+    public static void findElementAndClickWE(WebDriver driver, WebElement we, By locator) {
+        WebElement element = we.findElement(locator);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        element.click();
+    }
+
+    public static List<WebElement> findElements(WebDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+        return driver.findElements(locator);
+    }
+
+    public static void handleCaptcha() throws InterruptedException {
+        Thread.sleep(10000);  // Adjust the time as needed to handle CAPTCHA manually
+    }
 }
